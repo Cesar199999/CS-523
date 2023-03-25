3e1ed969f6969fa71c5691a58fba31e8aa55642e
cesar199999 <cesar.descalzo@epfl.ch>
2023-03-25 16:09:53 +0100

    Fix Beaver triplet generation and tests.

--- smcompiler/test_ttp.py
+++ smcompiler/test_ttp.py
@@ -5,73 +5,54 @@ Testing ttp is not obligatory.
 MODIFY THIS FILE.
 """
 
-from ttp import TrustedParamGenerator, BeaverTriplet
-from secret_sharing import Share
+from ttp import TrustedParamGenerator
 
 
-def test_nump():
+def test_constructor():
     ttp = TrustedParamGenerator()
-
-    ttp.add_participant('0')
-
-    ttp.add_participant('1')
-
-    ttp.add_participant('2')
+    assert ttp.participant_ids == set()
 
 
-def test_participants():
+def test_add_participant():
     ttp = TrustedParamGenerator()
-
-    ttp.add_participant('0')
-    ttp.add_participant('1')
-    ttp.add_participant('2')
-    ttp.add_participant('2')
-    ttp.add_participant('3')
-
-    assert ttp.participant_ids == {'0','1','2','3'}
-
-def test_beaver():
-    triplet = BeaverTriplet(3)
-
-    assert (triplet.a * triplet.b)%101 == triplet.c
-    sharea = Share(value=triplet.a)
-    shareb = Share(value=triplet.b)
-    sharec = Share(value=triplet.c)
-    product = sharea*shareb
-    assert product.value == sharec.value
-
-    a_1, b_1, c_1 = triplet.get_share_triplets(0)
-    a_2, b_2, c_2 = triplet.get_share_triplets(1)
-    a_3, b_3, c_3 = triplet.get_share_triplets(2)
-
-    sum_a = a_1 + a_2 + a_3
-    sum_b = b_1 + b_2 + b_3
-    sum_c = c_1 + c_2 + c_3
-
-    assert sum_a.value == Share(value = triplet.a).value
-    assert sum_b.value == Share(value = triplet.b).value
-    assert sum_c.value == Share(value = triplet.c).value
+    ttp.add_participant("1")
+    assert ttp.participant_ids == {"1"}
 
 
-def test_retrieveshare():
+def test_retrieve_share():
     ttp = TrustedParamGenerator()
+    _ = [ttp.add_participant(str(i)) for i in range(1, 4)]
+    share = ttp.retrieve_share("1", "op_id")
+    assert share == ttp.retrieve_share("1", "op_id")
+    assert share != ttp.retrieve_share("2", "op_id")
+    assert share != ttp.retrieve_share("3", "op_id")
 
-    ttp.add_participant('0')
-    ttp.add_participant('1')
-    ttp.add_participant('2')
 
-    assert ttp.client_number == {'0':0, '1':1, '2':2}
+def test_beaver_triplets_consistency_1():
+    ttp = TrustedParamGenerator()
+    _ = [ttp.add_participant(str(i)) for i in range(1, 4)]
+    shares = [ttp.retrieve_share(str(i), "op_id") for i in range(1, 4)]
+    [a, b, c] = [sum(col) for col in zip(*shares)]
+    assert (a * b).value == c.value
 
-    s0_0 = ttp.retrieve_share('0', 'add0')
-    s0_0_again = ttp.retrieve_share('0', 'add0')
 
-    assert s0_0 == s0_0_again 
+def test_beaver_triplets_consistency_2():
+    ttp = TrustedParamGenerator()
+    _ = [ttp.add_participant(str(i)) for i in range(1, 10)]
+    shares = [ttp.retrieve_share(str(i), "op_id") for i in range(1, 10)]
+    [a, b, c] = [sum(col) for col in zip(*shares)]
+    assert (a * b).value == c.value
 
-    s0_1 = ttp.retrieve_share('1', 'add0')
-    s0_2 = ttp.retrieve_share('2', 'add0')
 
-    assert (s0_0[0] + s0_1[0] + s0_2[0]) * (s0_0[1] + s0_1[1] + s0_2[1]) == (s0_0[2] + s0_1[2] + s0_2[2])
+def test_beaver_triplets_persisting_consistency():
+    ttp = TrustedParamGenerator()
+    _ = [ttp.add_participant(str(i)) for i in range(1, 10)]
+    shares = [ttp.retrieve_share(str(i), "op_id") for i in range(1, 10)]
+    [a, b, c] = [sum(col) for col in zip(*shares)]
+    assert (a * b).value == c.value
 
-    s1_0 = ttp.retrieve_share('0', 'mul1')
-
-    assert s0_0 != s1_0 
+    next_shares = [ttp.retrieve_share(str(i), "op_id") for i in range(1, 10)]
+    [next_a, next_b, next_c] = [sum(col) for col in zip(*next_shares)]
+    assert (next_a * next_b).value == next_c.value
+    assert [a.value, b.value, c.value] == [
+        next_a.value, next_b.value, next_c.value]
--- smcompiler/ttp.py
+++ smcompiler/ttp.py
@@ -4,35 +4,20 @@ Trusted parameters generator.
 MODIFY THIS FILE.
 """
 
-import collections
-from random import randint, sample
+from random import randint
 from typing import (
-    Dict,
     Set,
     Tuple,
 )
 
-from communication import Communication
-from secret_sharing import(
+from secret_sharing import (
+    UniquePrime,
     share_secret,
     Share,
 )
 
 # Feel free to add as many imports as you want.
 
-class BeaverTriplet:
-    """class holding beaver triplet with their shares"""
-
-    def __init__(self, num_participant):
-        self.a, self.b = sample(range(101), 2)
-        self.c = (self.a * self.b)%101
-
-        self.a_shares = share_secret(self.a, num_participant)
-        self.b_shares = share_secret(self.b, num_participant)
-        self.c_shares = share_secret(self.c, num_participant)
-    
-    def get_share_triplets(self, client_nb: int) -> Tuple[Share, Share, Share]:
-        return (self.a_shares[client_nb], self.b_shares[client_nb], self.c_shares[client_nb])
 
 class TrustedParamGenerator:
     """
@@ -41,27 +26,43 @@ class TrustedParamGenerator:
 
     def __init__(self):
         self.participant_ids: Set[str] = set()
-        self.num_participant = len(self.participant_ids)
-        self.operation_triplets = {}
-        self.client_number = {}
+        self.__operation_id_to_user_id_to_shares: dict[str,
+                                                       dict[str, Tuple[Share, Share, Share]]] = {}
 
     def add_participant(self, participant_id: str) -> None:
         """
         Add a participant.
         """
         self.participant_ids.add(participant_id)
-        self.client_number[participant_id] = len(self.participant_ids)-1
 
     def retrieve_share(self, client_id: str, op_id: str) -> Tuple[Share, Share, Share]:
         """
-        Retrieve a triplet of shares for a given client_id.
+        Retrieve a triplet of shares for a given client_id. The triplet (a, b, c) is such that a * b = c.
         """
+        if op_id not in self.__operation_id_to_user_id_to_shares:
+            self.__operation_id_to_user_id_to_shares[op_id] = self.generate_triplets(
+            )
 
-        if op_id not in self.operation_triplets:   
-            self.operation_triplets[op_id] = BeaverTriplet(len(self.participant_ids))
+        return self.__operation_id_to_user_id_to_shares[op_id][client_id]
 
-        triplet =  self.operation_triplets[op_id]
-        (a,b,c) = triplet.get_share_triplets(self.client_number[client_id])
-        return (a,b,c)
+    def generate_triplets(self) -> dict[Tuple[Share, Share, Share]]:
+        """
+        Generate a random triplet of shares for each participant.
+        """
+        [share_a, share_b] = [share_secret(randint(0, UniquePrime().get_unique_prime()), len(
+            self.participant_ids)) for _ in range(2)]
 
-    # Feel free to add as many methods as you want.
+        # Use the following formula to generate the third share:
+        # c = a * b = sum(a_i) * sum(b_j) = sum(a_i * sum(b_j)) =
+        #
+        # sum(a_i * sum(b_0 + b_1 + ... + b_i + ... + b_n)) =
+        #
+        # sum(a_i * b_i + a_i * (sum(b_0 + b_1 + ... + b_n) - b_i))
+        #
+        # Therefore,
+        # c_i = a_i * b_i + a_i * (sum(b_0 + b_1 + ... + b_n) - b_i)
+
+        share_c = [share_a[i] * share_b[i] + share_a[i] *
+                   (sum(share_b) - share_b[i]) for i in range(len(share_a))]
+
+        return {client_id: (share_a[i], share_b[i], share_c[i]) for i, client_id in enumerate(self.participant_ids)}
