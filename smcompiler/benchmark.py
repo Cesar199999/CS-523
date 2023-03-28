1e639988dd670de1cdf4c2405126c2114548cdfb
cesar199999 <cesar.descalzo@epfl.ch>
2023-03-28 19:20:57 +0200

    Reduce test complexity.

--- smcompiler/benchmark.py
+++ smcompiler/benchmark.py
@@ -38,7 +38,7 @@ def run_processes(expected, server_args, *client_args):
                for args in client_args]
 
     server.start()
-    time.sleep(4)
+    time.sleep(3)
     for client in clients:
         client.start()
 
@@ -53,7 +53,7 @@ def run_processes(expected, server_args, *client_args):
     server.join()
 
     # To "ensure" the workers are dead.
-    time.sleep(3)
+    time.sleep(2)
 
     print("Server stopped.")
 
@@ -174,12 +174,12 @@ def test_scalar_addition(nb_scalar_addition, nb_parties, benchmark):
 ### Run with python3 -m pytest benchmark.py -k 'test_scalar_multiplication' --benchmark-autosave --benchmark-sort=mean ###
 
 @pytest.mark.parametrize("nb_scalar_multiplication, nb_parties", [
-    (2, 2), (50, 2), (100, 2), (500, 2), (1000, 2), (2000, 2),
-    (2, 4), (50, 4), (100, 4), (500, 4), (1000, 4), (2000, 4),
-    (2, 8), (50, 8), (100, 8), (500, 8), (1000, 8), (2000, 8),
-    (2, 16), (50, 16), (100, 16), (500, 16), (1000, 16), (2000, 16),
-    (2, 32), (50, 32), (100, 32), (500, 32), (1000, 32), (2000, 32),
-    (2, 64), (50, 64), (100, 64), (500, 64), (1000, 64), (2000, 64)])
+    (2, 2), (50, 2), (100, 2), (500, 2), (1000, 2),
+    (2, 4), (50, 4), (100, 4), (500, 4), (1000, 4),
+    (2, 8), (50, 8), (100, 8), (500, 8), (1000, 8),
+    (2, 16), (50, 16), (100, 16), (500, 16), (1000, 16),
+    (2, 32), (50, 32), (100, 32), (500, 32), (1000, 32)
+])
 def test_scalar_multiplication(nb_scalar_multiplication, nb_parties, benchmark):
     parties, expr, expected = experiment_definition(bit_length=16,
                                                     nb_scalar_multiplication=nb_scalar_multiplication, nb_parties=nb_parties)
@@ -189,12 +189,11 @@ def test_scalar_multiplication(nb_scalar_multiplication, nb_parties, benchmark):
 ### Run with python3 -m pytest benchmark.py -k 'test_secret_addition' --benchmark-autosave --benchmark-sort=mean ###
 
 @pytest.mark.parametrize("nb_secret_addition, nb_parties", [
-    (2, 2), (50, 2), (100, 2), (500, 2), (1000, 2), (2000, 2),
-    (2, 4), (50, 4), (100, 4), (500, 4), (1000, 4), (2000, 4),
-    (2, 8), (50, 8), (100, 8), (500, 8), (1000, 8), (2000, 8),
-    (2, 16), (50, 16), (100, 16), (500, 16), (1000, 16), (2000, 16),
-    (2, 32), (50, 32), (100, 32), (500, 32), (1000, 32), (2000, 32),
-    (2, 64), (50, 64), (100, 64), (500, 64), (1000, 64), (2000, 64)
+    (2, 2), (50, 2), (100, 2), (500, 2), (1000, 2),
+    (2, 4), (50, 4), (100, 4), (500, 4), (1000, 4),
+    (2, 8), (50, 8), (100, 8), (500, 8), (1000, 8),
+    (2, 16), (50, 16), (100, 16), (500, 16), (1000, 16),
+    (2, 32), (50, 32), (100, 32), (500, 32), (1000, 32),
 ])
 def test_secret_addition(nb_secret_addition, nb_parties, benchmark):
     parties, expr, expected = experiment_definition(bit_length=16,
