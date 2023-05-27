483714c281d4c78dfcd4c002c8f8b29dd9d1cb5b
Voxanimus <louishenridanielpepito.dumas@epfl.ch>
2023-05-27 17:16:22 +0200

    classify code is working but not accurate enough

--- secretstroll/capture.py
+++ secretstroll/capture.py
@@ -1,15 +1,16 @@
 import subprocess
+from datetime import datetime
 
 # we iterate over all cells in the grid.
 for i in range(1, 101):
     # set the name of the trace to be registered
-    trace_name = "data_collection/trace_grid_{i}.pcap".format(i = str(i))
+    trace_name = "data_collection/trace_grid_{i}_{date}.pcap".format(i = str(i), date = datetime.now().strftime("%d_%H:%M:%S"))
     print("capturing "+ trace_name)
 
     # use tcpdump to intercept tcp packets and use -w options to read it
     # use -s options to save only the first 64 bytes (containing headers)
     # only intercept packets with tcp payloads with greater than 55
-    tcpdump = subprocess.Popen(['tcpdump', '-w', trace_name, '-s', '68', 'greater', '55', 'and', 'tcp'])
+    tcpdump = subprocess.Popen(['tcpdump', '-w', trace_name, '-s', '64', 'greater', '55', 'and', 'tcp'])
 
     # launch the query for dojo
     # the choice of dojo is motivated by the list of pois given in Part 2. since dojo seems to be the most frequent poi
